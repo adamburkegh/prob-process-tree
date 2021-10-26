48452e2631ec65096d139392fed460c875fa273b
Adam Burke <adamburkemail@gmail.com>
2021-10-26 11:46:55 +1000

    Initial import and adjustment to support repetitions on loops

---
+++ src/main/java/qut/pm/spm/ppt/PPTLoopImpl.java
@@ -0,0 +1,26 @@
+package qut.pm.spm.ppt;
+
+public class PPTLoopImpl extends PPTNodeImpl {
+
+	private double repetitions;
+	
+	public PPTLoopImpl(double repetitions) {
+		super(PPTOperator.PROBLOOP);
+		this.repetitions = repetitions;
+	}
+
+	@Override
+	public void addChild(ProbProcessTree child) {
+		if (children.isEmpty()) {
+			super.addChild(child);
+			return;
+		}
+		throw new ProcessTreeConsistencyException("Loops may only have one child");
+	}
+	
+	public double getLoopRepetitions() {
+		return repetitions;
+	}
+
+	
+}
